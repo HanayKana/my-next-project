39b583ae3195a463f0c23223d30b4fae23065a93
Kanako <kanakatori87@gmail.com>
2026-03-25 20:39:04 +0900

    6章まで完了

---
+++ app/news/[slug]/page.tsx
@@ -0,0 +1,29 @@
+import { notFound } from "next/navigation";
+import { getNewsDetail } from "@/app/_libs/microcms";
+import Article from "@/app/_components/Article";
+import styles from "./page.module.css";
+import ButtonLink from "@/app/_components/ButtonLink";
+
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+export default async function Page({ params }: Props) {
+  const { slug } = await params;
+  const data = await getNewsDetail(slug).catch(notFound);
+  return (
+    <>
+      <Article data={data} />
+      <div className={styles.footer}>
+        <ButtonLink href="/news">ニュース一覧へ</ButtonLink>
+      </div>
+    </>
+  );
+}
+
+// export default async function Page(props: Props) {
+//   const { slug } = await props.params;
+//   return <div>{slug}</div>;
+// }
